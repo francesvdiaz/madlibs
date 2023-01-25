f2371172739feadf71640d734aa96a27e410490b
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-01-25 15:06:33 -0800

    play around with js code layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    // in this section we get the value for each form input
+    event.preventDefault();
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
     const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
+    // Here's the first log we add.
+    console.log("verbInput = " + verbInput);
     const nounInput = document.getElementById("nounInput").value;
 
-    // then we set the story variables to the values we got from the form
     document.querySelector("span#person1a").innerText = person1Input;
     document.querySelector("span#person1b").innerText = person1Input;
     document.querySelector("span#person1c").innerText = person1Input;
     document.querySelector("span#person2a").innerText = person2Input;
     document.querySelector("span#person2b").innerText = person2Input;
     document.querySelector("span#animal").innerText = animalInput;
-    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#verb").inerText = verbInput;
     document.querySelector("span#noun").innerText = nounInput;
     document.querySelector("span#exclamation").innerText = exclamationInput;
 
     document.querySelector("div#story").removeAttribute("class");
-
-    event.preventDefault();
   };
 };
